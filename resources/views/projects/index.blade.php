a90b0c82caab1b03ba9cd7638817d9f9d6a73b7c
Christian Shannon <christian.shannon@reasondigital.com>
2021-08-21 17:28:29 +0100

    initial commit of community engagement platform dev

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -6,12 +6,20 @@
     </x-slot>
 
     <div class="py-12">
+
+        <a href="create/" class="h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">Create new project</a>
+
+
         @foreach($projects as $project)
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     
                         {{ $project->title }}
+
+                        <a href="{{ $project->id }}/" class="h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">View</a>
+
+                        <a href="{{ $project->id }}/edit" class="h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">Edit</a>
 
                 </div>
             </div>
